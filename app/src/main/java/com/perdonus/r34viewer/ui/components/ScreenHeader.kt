a493b472ddfd90ae66709761f6da13ba6cbbfa01
Perdonus <kala5ixa@gmail.com>
2026-03-19 23:46:42 +0300

    Fix Compose weight imports for CI

--- app/src/main/java/com/perdonus/r34viewer/ui/components/ScreenHeader.kt
+++ app/src/main/java/com/perdonus/r34viewer/ui/components/ScreenHeader.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
